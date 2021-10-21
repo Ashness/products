dddb5b290ead56e1d833ed6e7964f67ee2bfa3e4
Alex Yang <1453363089@qq.com>
2021-10-21 18:44:21 +0800

    update stm32 example

--- examples/STM32/USER/main.c
+++ examples/STM32/USER/main.c
@@ -1,7 +1,6 @@
 #include "delay.h"
 #include "usart.h"
-#include "imu_data_decode.h"
-#include "packet.h"
+#include "ch_serial.h"
  
 /************************************************
  ALIENTEK战舰STM32开发板实验
@@ -24,72 +23,78 @@
  官网：http://www.hipnuc.com
 ************************************************/
 
-void dump_data_packet(id0x91_t *data);     //打印数据
-void SysTick_Handler(void);                               //SysTick中断处理函数
-void SysTick_Init(void);                                  //SysTick初始化函数
 
-static uint32_t frame_rate;                               //获取帧频率
-static uint8_t usart1_output_flag;                        //向终端输出标志
+void SysTick_Handler(void); 
+void SysTick_Init(void); 
+
+static raw_t raw;                                         /* IMU stram read/control struct */
+static uint8_t decode_succ;                               /* 0: no new frame arrived, 1: new frame arrived */
+
+static void dump_imu_data(raw_t *raw)
+{
+    int i;
+    if(raw->item_code[0] != KItemGWSOL) /* HI222(HI221GW) */
+    {
+        printf("%-16s%d\r\n",       "id:",  raw->imu[0].id);
+        printf("%-16s%.3f %.3f %.3f\r\n",       "acc(G):",        raw->imu[0].acc[0], raw->imu[0].acc[1],  raw->imu[0].acc[2]);
+        printf("%-16s%.3f %.3f %.3f\r\n",       "gyr(deg/s):",    raw->imu[0].gyr[0], raw->imu[0].gyr[1],  raw->imu[0].gyr[2]);
+        printf("%-16s%.3f %.3f %.3f\r\n",       "mag(uT):",       raw->imu[0].mag[0], raw->imu[0].mag[1],  raw->imu[0].mag[2]);
+        printf("%-16s%.3f %.3f %.3f\r\n",       "eul(deg):",      raw->imu[0].eul[0], raw->imu[0].eul[1],  raw->imu[0].eul[2]);
+        printf("%-16s%.3f %.3f %.3f %.3f\r\n",  "quat:",          raw->imu[0].quat[0], raw->imu[0].quat[1],  raw->imu[0].quat[2], raw->imu[0].quat[3]);
+        printf("%-16s%.3f\r\n",       "presure(pa):",  raw->imu[0].pressure);
+        printf("%-16s%d\r\n",       "timestamp(ms):",  raw->imu[0].timestamp);
+        
+        printf("item: ");
+        for(i=0; i<raw->nitem_code; i++)
+        {
+            printf("0x%02X ", raw->item_code[i]);
+        }
+        printf("\r\n");
+    }
+    else /* HI226 HI229 CH100 CH110 */
+    {
+        printf("gateway: %s%d, %s%d\r\n",       "gwid:",      raw->gwid, "node cnt:", raw->nimu);
+        for(i=0; i<raw->nimu; i++)
+        {
+            printf("%-16s%d\r\n",       "id:",  raw->imu[i].id);
+            printf("%-16s%.3f %.3f %.3f\r\n",       "acc(G):",        raw->imu[i].acc[0], raw->imu[i].acc[1],  raw->imu[i].acc[2]);
+            printf("%-16s%.3f %.3f %.3f\r\n",       "gyr(deg/s):",    raw->imu[i].gyr[0], raw->imu[i].gyr[1],  raw->imu[i].gyr[2]);
+            printf("%-16s%.3f %.3f %.3f\r\n",       "mag(uT):",       raw->imu[i].mag[0], raw->imu[i].mag[1],  raw->imu[i].mag[2]);
+            printf("%-16s%.3f %.3f %.3f\r\n",       "eul(deg):",      raw->imu[i].eul[0], raw->imu[i].eul[1],  raw->imu[i].eul[2]);
+            printf("%-16s%.3f %.3f %.3f %.3f\r\n",  "quat:",          raw->imu[i].quat[0], raw->imu[i].quat[1],  raw->imu[i].quat[2], raw->imu[i].quat[3]);
+            printf("%-16s%.3f\r\n",       "presure(pa):",  raw->imu[i].pressure);
+            printf("%-16s%d\r\n",       "timestamp(ms):",  raw->imu[i].timestamp);
+        }
+    }
+}
+
 
 int main(void)
 {		
-    uint32_t i = 0;
-
     delay_init();	    	                               //延时函数初始化	  
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);        //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
     uart_init(115200);	                                   //串口初始化为115200
-  
-    imu_data_decode_init();                                //映射解析函数
-    SysTick_Init();                                        //开启SysTick，定时20ms
 
     while(1)
     {
-        if(usart1_output_flag)
+        if(decode_succ)
         {
-            usart1_output_flag = 0;
-            if(id0x62.tag != KItemGWSOL)
-			{
-				/* printf imu data packet */
-				dump_data_packet(&id0x91);
-				putchar(10);
-			}
-			else
-			{
-				/* printf gw data packet */
-				printf("        GW ID:  %-8d\n",id0x62.gw_id);
-				for(i = 0; i < id0x62.n; i++)
-				{ 
-					dump_data_packet(&id0x62.id0x91[i]);
-					puts("");
-				}
-			}
-		}
+            decode_succ = 0;
+            dump_imu_data(&raw);
+        }
 	}	 
 }
 
-/* 200ms interrupt */
-void SysTick_Handler(void)
-{
-	static uint32_t div;    
-	if(div == 5)
-	{
-		div = 0;
-		frame_rate = frame_count;
-		frame_count = 0;
-		usart1_output_flag = 1;
-	}
-    
-	div++;
-}
-
-/* serial usart2 interrupt functional */
-void USART2_IRQHandler(void)                	            //串口2中断服务程序
+void USART2_IRQHandler(void)
 {
 	uint8_t ch;
-	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)   //接收中断 
-		ch = USART_ReceiveData(USART2);	                    //读取接收到的数据
-
-	packet_decode(ch);                                      //解析数据
+	if(USART_GetITStatus(USART2, USART_IT_RXNE) != RESET)
+    {
+        ch = USART_ReceiveData(USART2);	
+    }
+    
+    /* decode each byte */
+    decode_succ = ch_serial_input(&raw, ch);
 } 
 
 /* SysTick initialization */
@@ -100,22 +105,4 @@ void SysTick_Init(void)
     SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk; 
 }
 
-/* printf hi229 or hi226 data packet*/
-void dump_data_packet(id0x91_t *data)
-{
-	if(bitmap & BIT_VALID_ID)
-		printf("    Device ID:  %-8d\r\n",  data->id);
-	printf("   Frame Rate: %4dHz\r\n", frame_rate);
-    if(bitmap & BIT_VALID_TIME)
-        printf(" Run Time(ms):  %d day %d:%d:%d:%d\r\n", data->ts / 86400000, data->ts / 3600000 % 24, data->ts / 60000 % 60, data->ts / 1000 % 60, data->ts / 1000);
-	if(bitmap & BIT_VALID_ACC)
-		printf("       Acc(G):	%8.3f %8.3f %8.3f\r\n",  data->acc[0],  data->acc[1],  data->acc[2]);
-	if(bitmap & BIT_VALID_GYR)
-		printf("   gyr(deg/s):	%8.2f %8.2f %8.2f\r\n",  data->gyr[0],  data->gyr[1],  data->gyr[2]);
-	if(bitmap & BIT_VALID_MAG)
-		printf("      mag(uT):	%8.2f %8.2f %8.2f\r\n",  data->mag[0],  data->mag[1],  data->mag[2]);
-	if(bitmap & BIT_VALID_EUL)
-		printf("   eul(R P Y):  %8.2f %8.2f %8.2f\r\n",  data->eul[0],  data->eul[1],  data->eul[2]);
-	if(bitmap & BIT_VALID_QUAT)
-		printf("quat(W X Y Z):  %8.3f %8.3f %8.3f %8.3f\r\n",  data->quat[0],  data->quat[1],  data->quat[2],  data->quat[3]);
-}
+
